7f3b99ef13fa75ecb8d34a056ef252dee2a02fc9
Thiyagesh <30830241+thiyageshece@users.noreply.github.com>
2018-03-15 19:51:19 +0530

    File creation

---
+++ Array/Array-15032018075137.java
@@ -0,0 +1,24 @@
+
+class Main {
+    public static void main(String args[] ) throws Exception {
+        /*
+         * Read input from stdin and provide input before running
+         * Use either of these methods for input
+
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        int N = Integer.parseInt(line);
+
+        //Scanner
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            System.out.println("hello world");
+        }
+        */
+
+        System.out.println("Hello World!");
+    }
+}
